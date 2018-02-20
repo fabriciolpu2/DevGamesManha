32fc0a3ac9663651f65f9fd8d90cff6e10874cb5
Lucas Medeiros de Souza <sr.lucart@gmail.com>
2018-02-20 18:01:53 -0300

    Melhoras no unity e player fps

---
+++ Orla/OrlaVr/Assets/Scripts/PlayerLook.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLook : MonoBehaviour {
+
+    public Transform playerBody;
+    public float SensibilidadeMouse;
+
+    float xAxisClamp = 0;
+
+    private void Awake()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void Update()
+    {
+        RotacionarCamera();
+    }
+
+    void RotacionarCamera()
+    {
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+
+        float rotAmountX = mouseX * SensibilidadeMouse;
+        float rotAmountY = mouseY * SensibilidadeMouse;
+
+        xAxisClamp -= rotAmountY;
+
+        Vector3 targetRotCam = transform.rotation.eulerAngles;
+        targetRotCam.x -= rotAmountY;
+
+        Vector3 targetRotBody = playerBody.rotation.eulerAngles;
+        targetRotBody.y += rotAmountX;
+
+        targetRotCam.z = 0;
+
+        if(xAxisClamp > 90){
+            xAxisClamp = 90;
+            targetRotCam.x = 90;
+        }else if(xAxisClamp < -90){
+            xAxisClamp = -90;
+            targetRotCam.x = 270;
+        }
+
+        transform.rotation = Quaternion.Euler(targetRotCam);
+        playerBody.rotation = Quaternion.Euler(targetRotBody);
+
+    }
+
+
+}
---
+++ Orla/OrlaVr/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour {
+
+    CharacterController charControl;
+    public float walkSpeed;
+
+    private void Awake()
+    {
+        charControl = GetComponent <CharacterController>();
+    }
+
+    private void Update()
+    {
+        MovePayer();
+    }
+
+    void MovePayer()
+    {
+        float horiz = Input.GetAxis("Horizontal");
+        float vert = Input.GetAxis("Vertical");
+
+        Vector3 moverDirSide = transform.right * horiz * walkSpeed;
+        Vector3 moveDirFoward = transform.forward * vert * walkSpeed;
+
+        charControl.SimpleMove(moverDirSide);
+        charControl.SimpleMove(moveDirFoward);
+
+    }
+}
