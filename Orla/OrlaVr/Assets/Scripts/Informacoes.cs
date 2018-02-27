9b85b2bc07b95b3fe18075cf861c3ffa83774697
JoaoLucas1 <oliveirajoao_lucas@hotmail.com>
2018-02-27 10:23:58 -0400

    vr

---
+++ Orla/OrlaVr/Assets/Scripts/Informacoes.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Informacoes : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Informacoes")
+        {
+            print("texto");
+        }
+
+    }
+}
