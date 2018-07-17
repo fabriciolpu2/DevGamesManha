2e81a86173209afea8b16957dbf955a07e241ed4
Fabricio Silva <fabriciolpu2@gmail.com>
2018-07-17 08:51:11 -0400

    Terceira pessoa

---
+++ Orla/VROrla/Assets/GameController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class GameController : MonoBehaviour
+{
+
+    public GameObject player;
+    public GameObject[] objetos;
+    public GameObject UIMenu;
+    public Text texto;
+    public Text txtItensCount;
+    public int itensCount = 0;
+    public int qtdTotalItens = 3;
+    // Use this for initialization
+    void Start() {
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (itensCount >= qtdTotalItens)
+        {
+            print("Fim Jogo");
+        }       
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "objeto")
+        {
+            // print("Dentro da area");
+            if (Input.GetButtonDown("Fire1"))
+            {
+                //texto.text("fd");
+                itensCount++;
+                txtItensCount.text = "Intens: " + itensCount;
+                texto.text = "Coletou um Obejto";
+                print("Interagiu");
+                Destroy(other.gameObject);
+                StartCoroutine(TempoTxt());                
+            }
+
+        }
+    }
+    public void Iniciar()
+    {
+        UIMenu.SetActive(false);
+    }
+    IEnumerator TempoTxt()
+    {
+        yield return new WaitForSeconds(3);
+        texto.text = "";
+    }
+
+    public void EscolherPersonagem()
+    {
+        new Vector3(Random.Range(-10.0f, 10.0f), 0, Random.Range(-10.0f, 10.0f));
+    }
+    public void Reiniciar()
+    {
+        //print("Reiniciar");
+        SceneManager.LoadScene(SceneManager.GetSceneAt(0).name);
+    }
+}
---
+++ Orla/VROrla/Assets/RealisticCarControllerV2/Scripts/Enter-Exit/RCCEnterExitPlayer.cs
@@ -0,0 +1,53 @@
+﻿//----------------------------------------------
+//            Realistic Car Controller
+//
+// Copyright © 2015 BoneCracker Games
+// http://www.bonecrackergames.com
+//
+//----------------------------------------------
+
+using UnityEngine;
+using System.Collections;
+
+public class RCCEnterExitPlayer : MonoBehaviour {
+	
+	public float maxRayDistance= 2.0f;
+	private bool showGui = false;
+	
+	void Update (){
+		
+		Vector3 direction= transform.TransformDirection(Vector3.forward);
+		RaycastHit hit;
+
+		if(Physics.Raycast(transform.position, direction, out hit, maxRayDistance)){
+
+			if(hit.transform.GetComponentInParent<RCCCarControllerV2>()){
+
+				showGui = true;
+				
+				if(Input.GetKeyDown(KeyCode.E))
+					hit.transform.root.SendMessage("Act", GetComponentInParent<CharacterController>().gameObject, SendMessageOptions.DontRequireReceiver);
+
+			}else{
+
+				showGui = false;
+
+			}
+			
+		}else{
+
+			showGui = false;
+
+		}
+		
+	}
+	
+	void OnGUI (){
+		
+		if(showGui){
+			GUI.Label( new Rect(Screen.width - (Screen.width/1.7f),Screen.height - (Screen.height/1.4f),800,100),"Press ''E'' key to Get In");
+		}
+		
+	}
+	
+}
